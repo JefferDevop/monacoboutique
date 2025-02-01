1a4f98df5b581407e70d1ba2fadaaf8bb758d725
JefferDevop <somadesarrollo@gmail.com>
2025-02-01 01:24:59 -0500

    generate alert when exist product whit stock in cero

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
-export const BASE_API = "http://testing.localhost:8000" 
-// export const BASE_API = "https://boutiquemonaco.workhard.site"
+// export const BASE_API = "http://testing.localhost:8000" 
+export const BASE_API = "https://boutiquemonaco.workhard.site"
 
 export const SEARCH_HISTORY = "search-history";
 export const CART = "cart_monaco";
